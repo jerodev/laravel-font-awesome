06ce9e66e14fe47996a407930aeae02aacf7031c
Jeroen Deviaene <jeroen@deviaene.eu>
2019-12-24 13:56:26 +0100

    Don't inject stylesheet on redirect responses

--- src/Middleware/InjectStyleSheet.php
+++ src/Middleware/InjectStyleSheet.php
@@ -3,8 +3,9 @@
 namespace Jerodev\LaraFontAwesome\Middleware;
 
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as SymfonyBaseResponse;
 
 final class InjectStyleSheet
 {
@@ -13,7 +14,8 @@ final class InjectStyleSheet
         $response = $next($request);
 
         if (
-            ($response->headers->has('Content-Type') && \strpos($response->headers->get('Content-Type'), 'html') === false)
+            $response instanceof RedirectResponse
+            || ($response->headers->has('Content-Type') && \strpos($response->headers->get('Content-Type'), 'html') === false)
             || $request->getRequestFormat() !== 'html'
             || $response->getContent() === false
             || $request->isXmlHttpRequest()
@@ -24,7 +26,7 @@ final class InjectStyleSheet
         return $this->injectStyleSheet($response);
     }
 
-    private function injectStyleSheet(Response $response)
+    private function injectStyleSheet(SymfonyBaseResponse $response)
     {
         $content = $response->getContent();
         $content = \str_replace('</head>', '<link rel="stylesheet" href="https://unpkg.com/@fortawesome/fontawesome-free@5.12.0/css/svg-with-js.min.css" /></head>', $content);
---
+++ tests/MiddlewareTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Jerodev\LaraFontAwesome\Tests;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Jerodev\LaraFontAwesome\Middleware\InjectStyleSheet;
+use Symfony\Component\HttpFoundation\Response as SymfonyBaseResponse;
+
+class MiddlewareTest extends TestCase
+{
+    /** @var InjectStyleSheet */
+    private $middleware;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->middleware = new InjectStyleSheet();
+    }
+
+    public function testDontInjectOnAjaxRequest(): void
+    {
+        $request = new Request();
+        $request->headers->add(['X-Requested-With' => 'XMLHttpRequest']);
+        $next = static function () {
+            return new Response('<html><head></head></html>');
+        };
+
+        $response = $this->middleware->handle($request, $next);
+
+        $this->assertStylesheetInjected($response, false);
+    }
+
+    public function testDontInjectOnRedirect(): void
+    {
+        $request = new Request();
+        $next = static function () {
+            return new RedirectResponse('https://www.deviaene.eu/');
+        };
+
+        $response = $this->middleware->handle($request, $next);
+
+        $this->assertStylesheetInjected($response, false);
+    }
+
+    public function testInjectsStylesheetInHtmlResponse(): void
+    {
+        $request = new Request();
+        $next = static function () {
+            return new Response('<html><head></head></html>');
+        };
+
+        $response = $this->middleware->handle($request, $next);
+
+        $this->assertStylesheetInjected($response);
+    }
+
+    private function assertStylesheetInjected(SymfonyBaseResponse $response, bool $injected = true): void
+    {
+        if ($injected) {
+            $this->assertStringContainsString(
+                '@fortawesome',
+                $response->getContent()
+            );
+        } else {
+            $this->assertStringNotContainsString(
+                '@fortawesome',
+                $response->getContent()
+            );
+        }
+    }
+}
